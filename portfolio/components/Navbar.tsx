b940ef1d63f84fdb464b3cc70b3af220331b6691
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2022-02-10 14:24:31 -0100

    style the navbar

--- portfolio/components/Navbar.tsx
+++ portfolio/components/Navbar.tsx
@@ -1,9 +1,54 @@
 import * as React from 'react';
+import Image from 'next/image';
+import profilePic from '../public/profile.jpg';
 
 const Navbar: React.FunctionComponent = () => {
 	return (
-		<div>
-			<h1 className="text-4xl text-center font-bold text-gray-900">Nav bar</h1>
+		<div className="flex flex-col items-center">
+			<h1 className="font- text-2xl text-center font-bold pt-4 capitalize">
+				<a href="#" className="">
+					Moro owusu Afriyie
+				</a>
+			</h1>
+			<div className="navbar-menu py-6 px-4 w-full flex col gap-3 items-center">
+				<div className="profile-section w-full flex flex-col gap-3 items-center">
+					<Image
+						src={profilePic}
+						alt="profile picture"
+						width={160}
+						height={160}
+						className="profile-image rounded-full object-cover"
+					/>
+					<div className="bio text-sm text-center">
+						Hi, my name is Moro Owusu Afriyie and I&apos;m a fullStack developer👩‍💻. Welcome to my
+						personal website😊!
+					</div>
+					<ul className="social-list py-2 mx-auto flex gap-4">
+						<li className="list-item">
+							<a href="#">
+								<i className="fa fa-twitter" aria-hidden="true"></i>
+							</a>
+						</li>
+						<li className="list-item">
+							<a href="#">
+								<i className="fa fa-linkedin" aria-hidden="true"></i>
+							</a>
+						</li>
+						<li className="list-item">
+							<a href="#">
+								<i className="fa fa-github" aria-hidden="true"></i>
+							</a>
+						</li>
+						<li className="list-item">
+							<a href="#">
+								<i className="fa fa-stack-overflow" aria-hidden="true"></i>
+							</a>
+						</li>
+					</ul>
+					<hr className=" h-px w-full border-white opacity-25" />
+				</div>
+				<div className="navbar-nav"></div>
+			</div>
 		</div>
 	);
 };
--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
 			backgroundImage: {
 				'blue-gradient': 'linear-gradient(to bottom, #3b7eeb, #0094ee, #00a6e9, #15b6df, #5bc3d5)',
 			},
+			fontFamily: {
+				poppins: "'Poppins', sans-serif",
+			},
 		},
 	},
 	plugins: [],
